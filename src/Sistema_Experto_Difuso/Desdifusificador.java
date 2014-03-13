adfedf046779318dda8920d6c3328eec7ce13283
ACuadrosA <acuadrosb1990@hotmail.com>
2014-03-13 12:11:24 -0600

    Update Desdifusificador.java

--- src/Sistema_Experto_Difuso/Desdifusificador.java
+++ src/Sistema_Experto_Difuso/Desdifusificador.java
@@ -16,4 +16,35 @@ public class Desdifusificador
     {
         
     }
+    
+    /*
+    Este método genera el archivo que contiene las variables de salida, arch
+    es el archivo de la variable de salida que se seleccionará, etiqueta es el
+    nombre de la etiqueta y valor el valor de la etiqueta
+   */
+    
+    public static void generarArchivo(String arch,String etiqueta,double valor)
+    {
+     /*
+      El símbolo | indica la separación entre la etiqueta y su valor, y el
+      caracter / indica fin de línea
+        */   
+     try
+     {
+      RandomAccessFile raf=new RandomAccessFile(arch,"rw");
+      raf.seek(raf.length());
+      raf.writeChars(etiqueta);
+      raf.writeChar('|');
+      raf.writeDouble(valor);
+      raf.writeChar('/');
+     }//fin de try
+     catch(FileNotFoundException e)
+     {
+         System.out.println(e);
+     }//fin de catch
+     catch(IOException e)
+     {
+         System.out.println(e);
+     }//fin de catch    
+    }//Fin de generarArchivo
 }
